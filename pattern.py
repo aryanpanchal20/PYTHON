a688e71c9f27785b5f6bee666695ac64fe2c5c95
Aryan Panchal <panchalaryan946@gmail.com>
2026-09-18 16:51:00 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,22 +1,43 @@
+# # # i=1
+# # # while i<=3:
+# # #     j=1
+# # #     while j<=3:
+# # #         print("*" ,end=" ")
+# # #         j+=1
+# # #     print()
+# # #     i+=1
+
+# # # print()
+
+# # # for i in range(1,5):
+# # #     for j in range(1,5):
+# # #         print("*",end=" ")
+# # #     print()
+
+
 # # i=1
 # # while i<=3:
 # #     j=1
-# #     while j<=3:
-# #         print("*" ,end=" ")
+# #     while j<=i:
+# #         print("*",end=" ")
 # #         j+=1
 # #     print()
 # #     i+=1
 
 # # print()
 
-# # for i in range(1,5):
-# #     for j in range(1,5):
+# # for i in range(1,4):
+# #     for j in range(1,4):
 # #         print("*",end=" ")
 # #     print()
 
-
+# n=4
 # i=1
-# while i<=3:
+# while i<=n:
+#     k=1
+#     while k<=n-i:
+#         print(" ",end="")
+#         k+=1
 #     j=1
 #     while j<=i:
 #         print("*",end=" ")
@@ -26,35 +47,37 @@
 
 # print()
 
-# for i in range(1,4):
-#     for j in range(1,4):
+# for i in range(1,n+1):
+#     for k in range(1,((n-i)+1)):
+#         print(" ",end=" ")
+#     for j in range(1,i+1):
 #         print("*",end=" ")
 #     print()
 
-n=4
+#     print()
+
+# for i in range(1,n+1):
+#     print(" "*(n-i)+"*"*i)
+
+
+# i=1
+# while i<=3:
+#     j=1
+#     while j<=3:
+#         print("*" ,end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+
 i=1
-while i<=n:
-    k=1
-    while k<=n-i:
-        print(" ",end="")
-        k+=1
+ch=97
+
+while i<=3:
     j=1
     while j<=i:
-        print("*",end=" ")
+        print(chr(ch),end=" ")
+        ch+=1
         j+=1
     print()
     i+=1
-
-print()
-
-for i in range(1,n+1):
-    for k in range(1,((n-i)+1)):
-        print(" ",end=" ")
-    for j in range(1,i+1):
-        print("*",end=" ")
-    print()
-
-    print()
-
-for i in range(1,n+1):
-    print(" "*(n-i)+"*"*i)
