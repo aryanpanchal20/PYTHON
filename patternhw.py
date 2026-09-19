a7e79e520e86f9078574ad1ba76da95cff8e68f6
Aryan Panchal <panchalaryan946@gmail.com>
2026-09-19 16:44:38 +0530

    Update patternhw.py

--- patternhw.py
+++ patternhw.py
@@ -135,3 +135,17 @@ for i in range(n - 1, 0, -1):
             print(" ", end="")
 
     print()
+
+print("============================================================================================")
+
+n = 9
+
+for i in range(n):
+    for j in range(n):
+
+        if (j >= i and j <= n-i-1) or (j <= i and j >= n-i-1):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+
+    print()
